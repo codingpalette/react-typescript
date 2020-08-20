995deaa04743978fd45c8db8c232af075c3671a6
codingpalette <codingpalette@gmail.com>
2020-08-20 11:45:34 +0900

    word relay create

--- client.tsx
+++ client.tsx
@@ -4,7 +4,8 @@ import { hot } from 'react-hot-loader/root';
 
 import App from './components/App'
 import GuGuDan from "./components/GuGuDan";
+import WordRelay from "./components/WordRelay";
 
-const Hot = hot(GuGuDan);
+const Hot = hot(WordRelay); // HOC
 
 ReactDOM.render(<Hot /> , document.querySelector('#root'));
