1bed034a20adfb5c69a7b7720e029ca28bd37b50
codingpalette <codingpalette@gmail.com>
2020-08-25 11:02:58 +0900

    RSP modify

--- components/RSP.tsx
+++ components/RSP.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useState, useRef, useEffect } from 'react';
+import {useState, useRef, useEffect, useCallback} from 'react';
 import styled from '@emotion/styled';
 
 const Computer = styled('div')`
@@ -12,21 +12,67 @@ const rspCoords = {
     바위 : '0',
     가위 : '-142px',
     보 : '-284px'
-}
+} as const;
 
 const scores = {
     가위 : 1,
     바위 : 0,
     보 : -1
-}
+} as const;
 
-const computerChoice = (imgCoords) => {
-    return Object.keys(rspCoords).find((k) => {
+type ImgCoords = typeof rspCoords[keyof typeof rspCoords];
+// type imgCoords = '0' | '-142px' | '-284px';
+
+const computerChoice = (imgCoords : ImgCoords) => {
+    return (Object.keys(rspCoords) as ['바위', '가위', '보']).find((k) => {
         return rspCoords[k] === imgCoords;
-    })
+    })!
 }
 
 const RSP = () => {
+    const [result, setResult] = useState('');
+    const [imgCoord, setImgCoord] = useState<ImgCoords>(rspCoords.바위);
+    const [score, setScore] = useState(0);
+    const interval = useRef<number>();
+
+    useEffect(() => { // componentDidMount, componentDidUpdate 역할(1대1 대응은 아님)
+        console.log('다시 실행');
+        interval.current = window.setInterval(changeHand, 100);
+        return () => { // componentWillUnmount 역할
+            console.log('종료');
+            clearInterval(interval.current);
+        }
+    }, [imgCoord]);
+
+    const changeHand = () => {
+        if (imgCoord === rspCoords.바위) {
+            setImgCoord(rspCoords.가위);
+        } else if (imgCoord === rspCoords.가위) {
+            setImgCoord(rspCoords.보);
+        } else if (imgCoord === rspCoords.보) {
+            setImgCoord(rspCoords.바위);
+        }
+    };
+
+    const onClickBtn = (choice: keyof typeof rspCoords) => () => {
+        clearInterval(interval.current);
+        const myScore = scores[choice];
+        const cpuScore = scores[computerChoice(imgCoord)];
+        const diff = myScore - cpuScore;
+        if (diff === 0) {
+            setResult('비겼습니다!');
+        } else if ([-1, 2].includes(diff)) {
+            setResult('이겼습니다!');
+            setScore((prevScore) => prevScore + 1);
+        } else {
+            setResult('졌습니다!');
+            setScore((prevScore) => prevScore - 1);
+        }
+        setTimeout(() => {
+            interval.current = window.setInterval(changeHand, 100);
+        }, 1000);
+    }
+
     return(
         <>
             <Computer id="computer" style={{ background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0` }} />
